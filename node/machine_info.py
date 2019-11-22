125bac249ce9fe4abad89287b98cbb413e40e901
Shivam Waghela <shivamwaghela@gmail.com>
2019-11-22 13:04:08 -0800

    Add machine info module

---
+++ node/machine_info.py
@@ -0,0 +1,16 @@
+"""Information about the host machine"""
+
+import socket
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+
+    return IP
