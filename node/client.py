3704e337774eb6d7c0cbc2ef75efa645486c7823
Shivam Waghela <shivamwaghela@gmail.com>
2019-11-23 13:24:04 -0800

    Add network manager
    connect to server node in network and get the connection list

--- node/client.py
+++ node/client.py
@@ -1,25 +1,38 @@
 import grpc
 import logging
+import sys
+import os
+sys.path.append("../" + os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/generated/')
 
 import machine_info
 import greet_pb2
 import greet_pb2_grpc
+import network_manager_pb2
+import network_manager_pb2_grpc
 
 
 def greet():
-    node_ip = machine_info.get_ip()
-    node_port = 2750
-    channel = grpc.insecure_channel(node_ip + ":" + str(node_port))
-    stub = greet_pb2_grpc.GreeterStub(channel)
-    response = stub.SayHello(greet_pb2.HelloRequest(name=machine_info.get_ip()))
-
+    greeter_stub = greet_pb2_grpc.GreeterStub(channel)
+    response = greeter_stub.SayHello(greet_pb2.HelloRequest(name=machine_info.get_ip()))
     logger.info("Response from " + machine_info.get_ip() + ": " + response.message)
+    print("Response from " + machine_info.get_ip() + ": " + response.message)
+
+
+def get_connection_list():
+    network_manager_stub = network_manager_pb2_grpc.NetworkManagerStub(channel)
+    response = network_manager_stub.GetConnectionList(network_manager_pb2
+                                                      .GetConnectionListRequest(node_ip=machine_info.get_ip()))
+    print(response.node_ip)
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger(machine_info.get_ip())
-    logger.setLevel(logging.DEBUG)
     logging.basicConfig(filename='client.log', filemode='w',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+    logger = logging.getLogger(machine_info.get_ip())
+    logger.setLevel(logging.DEBUG)
+    node_ip = machine_info.get_ip()
+    node_port = 2750
+    channel = grpc.insecure_channel(node_ip + ":" + str(node_port))
     greet()
+    get_connection_list()
--- node/server.py
+++ node/server.py
@@ -1,25 +1,46 @@
 from concurrent import futures
 from yaml import load, Loader
 
+
 import grpc
 import logging
+import os
+import sys
+sys.path.append("../" + os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/generated/')
+
 
 import machine_info
 import greet_pb2
 import greet_pb2_grpc
+import network_manager_pb2
+import network_manager_pb2_grpc
+
+
+connection_list = []
 
 
 class Greeter(greet_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
         logger.info("Greetings received from " + request.name)
+        if request.name not in connection_list:
+            connection_list.append(request.name)
         return greet_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 
+class NetworkManager(network_manager_pb2_grpc.NetworkManagerServicer):
+
+    def GetConnectionList(self, request, context):
+        logger.info("GetConnectionList called from: " + request.node_ip)
+        return network_manager_pb2.GetConnectionListResponse(node_ip=connection_list)
+
+
 def serve():
-    config = load(open('../config.yaml'), Loader=Loader)
+    config = load(open('config.yaml'), Loader=Loader)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=50))
     greet_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    network_manager_pb2_grpc.add_NetworkManagerServicer_to_server(NetworkManager(), server)
     server.add_insecure_port(machine_info.get_ip() + ":" + str(config["port"]))
     logger.info("Server starting at port " + str(config["port"]))
     server.start()
@@ -27,7 +48,8 @@ def serve():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='server.log', filemode='w',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(machine_info.get_ip())
     logger.setLevel(logging.DEBUG)
-    logging.basicConfig(filename='server.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     serve()
