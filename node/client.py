4c7219449ccb080f8c6b8f2fa7e7b96a3edbea6c
Shivam Waghela <shivamwaghela@gmail.com>
2019-11-23 14:44:49 -0800

    send greetings to other nodes in network

--- node/client.py
+++ node/client.py
@@ -11,19 +11,28 @@ import greet_pb2_grpc
 import network_manager_pb2
 import network_manager_pb2_grpc
 
+connection_list = []
 
-def greet():
+
+def greet(channel):
     greeter_stub = greet_pb2_grpc.GreeterStub(channel)
     response = greeter_stub.SayHello(greet_pb2.HelloRequest(name=machine_info.get_ip()))
     logger.info("Response from " + machine_info.get_ip() + ": " + response.message)
-    print("Response from " + machine_info.get_ip() + ": " + response.message)
 
 
 def get_connection_list():
+    global connection_list
     network_manager_stub = network_manager_pb2_grpc.NetworkManagerStub(channel)
     response = network_manager_stub.GetConnectionList(network_manager_pb2
                                                       .GetConnectionListRequest(node_ip=machine_info.get_ip()))
-    print(response.node_ip)
+    connection_list = response.node_ip
+
+
+def greet_the_team():
+    for ip in connection_list:
+        if ip != machine_info.get_ip():
+            chn = grpc.insecure_channel(ip + ":" + str(node_port))
+            greet(chn)
 
 
 if __name__ == '__main__':
@@ -31,8 +40,9 @@ if __name__ == '__main__':
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(machine_info.get_ip())
     logger.setLevel(logging.DEBUG)
-    node_ip = machine_info.get_ip()
+    node_ip = "10.0.0.2"
     node_port = 2750
     channel = grpc.insecure_channel(node_ip + ":" + str(node_port))
-    greet()
+    greet(channel)
     get_connection_list()
+    greet_the_team()
