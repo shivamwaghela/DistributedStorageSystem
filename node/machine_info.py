71e32f85ff2b9a34657a11ddf51f5fca0ef77521
Shivam Waghela <shivamwaghela@gmail.com>
2019-11-23 14:19:39 -0800

    Fix get_ip

--- node/machine_info.py
+++ node/machine_info.py
@@ -6,7 +6,7 @@ import socket
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(('10.255.255.255', 1))
+        s.connect(('10.0.0.0', 1))
         IP = s.getsockname()[0]
     except:
         IP = '127.0.0.1'
