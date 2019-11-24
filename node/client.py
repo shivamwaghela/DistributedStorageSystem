ff92733eaac5fb99e8e538fc2056ad562694ff42
Shivam Waghela <shivamwaghela@gmail.com>
2019-11-23 17:07:07 -0800

    Add machine usage gRPC service; store connection list to file

--- node/client.py
+++ node/client.py
@@ -12,6 +12,7 @@ import network_manager_pb2
 import network_manager_pb2_grpc
 
 connection_list = []
+file = open("connection_list.txt", "r")
 
 
 def greet(ip, channel):
@@ -29,6 +30,8 @@ def get_connection_list():
 
 
 def greet_the_team():
+    global connection_list
+    connection_list = file.readlines()
     for ip in connection_list:
         if ip != machine_info.get_ip():
             chn = grpc.insecure_channel(ip + ":" + str(node_port))
--- node/server.py
+++ node/server.py
@@ -1,5 +1,6 @@
 from concurrent import futures
 from yaml import load, Loader
+from psutil import cpu_percent, virtual_memory, disk_usage
 
 
 import grpc
@@ -15,17 +16,24 @@ import greet_pb2
 import greet_pb2_grpc
 import network_manager_pb2
 import network_manager_pb2_grpc
+import machine_stats_pb2
+import machine_stats_pb2_grpc
 
 
-connection_list = [machine_info.get_ip()]  # add yourself to the list
+connection_list = []  # add yourself to the list
+file = open("connection_list.txt", "w")
+file.write(machine_info.get_ip())
 
 
 class Greeter(greet_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
+        global connection_list
         logger.info("Greetings received from " + request.name)
+        connection_list = file.readlines()
         if request.name not in connection_list:
-            connection_list.append(request.name)
+            file.write(request.name)
+        file.close()
         return greet_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 
@@ -34,6 +42,27 @@ class NetworkManager(network_manager_pb2_grpc.NetworkManagerServicer):
     def GetConnectionList(self, request, context):
         logger.info("GetConnectionList called from: " + request.node_ip)
         return network_manager_pb2.GetConnectionListResponse(node_ip=connection_list)
+
+
+class MachineState(machine_stats_pb2_grpc.MachineStatsServicer):
+
+    def GetCPUUsage(self, request, context):
+        logger.info("GetCPUUsage called from: " + request.node_ip)
+        cpu_usage = cpu_percent(interval=5)
+        logger.info("CPU utilization: " + cpu_usage)
+        return machine_stats_pb2.GetCPUUsageResponse(cpu_usage=cpu_usage)
+
+    def GetMemoryUsage(self, request, context):
+        logger.info("GetMemoryUsage called from: " + request.node_ip)
+        memory_usage = virtual_memory()
+        logger.info("Memory utilization: " + memory_usage)
+        return machine_stats_pb2.GetMemoryUsageResponse(memory_usage=memory_usage)
+
+    def GetDiskUsage(self, request, context):
+        logger.info("GetDiskUsage called from: " + request.node_ip)
+        hdd_usage = disk_usage("/")
+        logger.info("Disk utilization: " + hdd_usage)
+        return machine_stats_pb2.GetCPUUsageResponse(disk_usage=hdd_usage)
 
 
 def serve():
