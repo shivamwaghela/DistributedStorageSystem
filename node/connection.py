69e2dc1d0d6801e24584b503c584a030d0396aa9
Shivam Waghela <shivamwaghela@gmail.com>
2019-12-04 10:08:34 -0800

    Create object to maintain node connections
    Store established channels for later reuse

---
+++ node/connection.py
@@ -0,0 +1,23 @@
+class Connection:
+    """
+    Stores connection information
+    channel: gRPC channel
+    node_position: One of the position from Enum (TOP/BOTTOM/LEFT/RIGHT)
+    node_coordinates: tuple containing x & y coordinates
+    """
+    def __init__(self, channel, node_position, node_coordinates):
+        self._channel = channel
+        self._node_position = node_position
+        self._node_coordinates = node_coordinates
+
+    @property
+    def channel(self):
+        return self._channel
+
+    @property
+    def node_position(self):
+        return self._node_position
+
+    @property
+    def node_coordinates(self):
+        return self._node_coordinates
---
+++ node/node_connections.py
@@ -0,0 +1,61 @@
+import grpc
+
+
+class NodeConnections:
+    """
+    Stores all node connections
+    """
+    MAX_CONNECTIONS = 4
+
+    def __init__(self):
+        self.connection_dict = {}
+
+    def is_full(self):
+        """
+        Checks if the connections have reached capacity
+        :return: True if length of connection_list equals MAX_CONNECTIONS
+                else False
+        """
+        if len(self.connection_dict) < self.MAX_CONNECTIONS:
+            return False
+        else:
+            return True
+
+    def add_connection(self, connection):
+        """
+        Adds node connection
+        :param connection: Connection object
+        :return: Returns True if connection was added successfully, False otherwise
+        """
+        if connection.node_position in self.connection_dict:
+            return False
+
+        self.connection_dict[connection.node_position] = connection
+        return True
+
+    def remove_connection(self, node_position):
+        """
+        Adds node connection
+        :param node_position: Connection position (Left/Right/Top/Bottom)
+        :return: Returns True if connection was removed successfully, False otherwise
+        """
+        if node_position in self.connection_dict:
+            del self.connection_dict[node_position]
+            return True
+
+        return False
+
+    def get_connection_status(self):
+        """
+        Checks status of channels
+        :return: Dictionary containing status of connections
+        """
+        connection_status = {}
+        for node_position, connection in self.connection_dict:
+            try:
+                grpc.channel_ready_future(connection.channel).result(timeout=1)
+            except grpc.FutureTimeoutError:
+                connection_status[node_position] = False
+            connection_status[node_position] = True
+
+        return connection_status
