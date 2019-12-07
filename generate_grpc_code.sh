6fda42e3563585ef2fb58df103054d1190bc3c65
Drusti Thakkar <drustihitesh.thakkar@sjsu.edu>
2019-12-06 23:38:50 -0800

    adding traversal code

--- generate_grpc_code.sh
+++ generate_grpc_code.sh
@@ -12,6 +12,9 @@ then
 
     python -m grpc_tools.protoc -I./protos --python_out=./node/generated \
         --grpc_python_out=./node/generated ./protos/network_manager.proto
+
+    python -m grpc_tools.protoc -I./protos --python_out=./node/generated \
+            --grpc_python_out=./node/generated ./protos/traversal.proto
 else
     python3 -m grpc_tools.protoc -I./protos --python_out=./node/generated \
         --grpc_python_out=./node/generated ./protos/greet.proto
@@ -21,4 +24,7 @@ else
 
     python3 -m grpc_tools.protoc -I./protos --python_out=./node/generated \
         --grpc_python_out=./node/generated ./protos/network_manager.proto
+
+    python3 -m grpc_tools.protoc -I./protos --python_out=./node/generated \
+        --grpc_python_out=./node/generated ./protos/traversal.proto
 fi
