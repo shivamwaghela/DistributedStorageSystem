063bbbfd9805647292e3713c13685c743b44f2b8
Shivam Waghela <shivamwaghela@gmail.com>
2019-12-13 12:27:29 -0800

    Add locks to shared resource - NodeConnections object

--- node/globals.py
+++ node/globals.py
@@ -2,6 +2,7 @@
 Global variables file
 """
 
+import threading
 import machine_info
 from node_connections import NodeConnections
 from node_position import NodePosition
@@ -11,15 +12,17 @@ my_ip = None
 my_position = None
 my_coordinates = None
 node_connections = None
+lock = None
 
 
 def init():
     """
     Definitions of global variables that are used across modules
     """
-    global port, my_ip, my_position, my_coordinates, node_connections
+    global port, my_ip, my_position, my_coordinates, node_connections, lock
     port = 2750
     my_ip = machine_info.get_my_ip()
     my_position = NodePosition.CENTER
     my_coordinates = None
     node_connections = NodeConnections()
+    lock = threading.Lock()
