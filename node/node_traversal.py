faa97b96300892afeccad34237b395fbaeb5faff
Drusti Thakkar <drustihitesh.thakkar@sjsu.edu>
2019-12-14 14:01:12 -0800

    BFS implementation

--- node/node_traversal.py
+++ node/node_traversal.py
@@ -32,8 +32,8 @@ def fetch_data(hash_id):
 class Traversal(traversal_pb2_grpc.TraversalServicer):
     def ReceiveData(self, request, context):
         TraversalResponseStatus = traversal_response_status.TraversalResponseStatus
-        logger.info("Traversal.ReceiveData hash_id:{} request_id:{} stack:{} visited:{}"
-                    .format(request.hash_id, request.request_id, request.stack, request.visited))
+        logger.info("Traversal.ReceiveData hash_id:{} request_id:{} visited:{}"
+                    .format(request.hash_id, request.request_id, request.visited))
         # Check if the file exits on current node
         if True:
             curr_data = fetch_data(request.hash_id)
@@ -44,37 +44,24 @@ class Traversal(traversal_pb2_grpc.TraversalServicer):
             return traversal_pb2.ReceiveDataResponse(status = traversal_response_status.FOUND)
 
         # add neighbors to stack. before adding check if neighbor is already visited.
-        stack = eval(request.stack)
         visited = eval(request.visited)
 
-        if len(stack) == 0 and len(visited) != 0:
-            logger.info("Traversal.ReceiveData: len(stack) == 0 and len(visited) != 0")
-            # visited all the nodes but file not found
-            return traversal_pb2.ReceiveDataResponse(file_bytes=None,
-                                                     request_id=request.request_id,
-                                                     node_ip=globals.my_ip,
-                                                     status=TraversalResponseStatus.NOT_FOUND)
-
         # forward the request
-        visited.append(globals.my_ip)
+        if globals.my_ip not in visited:
+            visited.append(globals.my_ip)
         logger.info("Traversal.ReceiveData: visited: {}".format(visited))
 
         # add neighbors to stack
         for item in globals.node_connections.connection_dict.items():
-            if item[1].node_ip not in visited:
-                stack.append(item[1].node_ip) # items = {TOP: conn obj}
-
-        logger.info("Traversal.ReceiveData: stack: {}".format(stack))
-        forwarded_node_ip = ""
-        while len(stack) > 0:
-            forwarded_node_ip = stack.pop()
-            if forwarded_node_ip not in visited:
-                break
-
-        logger.info("Traversal.ReceiveData: forwarded_node_ip: {}".format(forwarded_node_ip))
-
-        forward_request_thread = threading.Thread(target=forward_receive_data_request, args=(forwarded_node_ip, request))
-        forward_request_thread.start()
+            if item[1].node_ip not in visited and item[1].channel.isAlive(): #check if node is not visited and channel is alive
+                visited.append(item[1].node_ip)
+        
+        for item in globals.node_connections.connection_dict.items():
+            if item[1].node_ip not in visited and item[1].channel.isAlive():
+                forwarded_node_ip = item[1].node_ip
+                channel = item[1].channel
+                forward_request_thread = threading.Thread(target=forward_receive_data_request, args=(forwarded_node_ip, channel, request))
+                forward_request_thread.start()
 
         return traversal_pb2.ReceiveDataResponse(file_bytes=None,
                                                  request_id=str(request.request_id),
@@ -82,93 +69,88 @@ class Traversal(traversal_pb2_grpc.TraversalServicer):
                                                  status=str(TraversalResponseStatus.FORWARDED))
 
     def RespondData(self, request, context):
-            t = threading.Thread(target=forward_response_data, args=(request.file_bytes, request.request_id, request.node_ip, request.status, request.path))
-            t.start()
-            return traversal_pb2.ResponseDataResponse(status = traversal_response_status.FORWARDED)
+        t = threading.Thread(target=forward_response_data, args=(request.file_bytes, request.request_id, request.node_ip, request.status, request.path))
+        t.start()
+        return traversal_pb2.ResponseDataResponse(status = traversal_response_status.FORWARDED)
 
 
 
-# XXX
-def forward_receive_data_request(node_ip, request):
-    logger.info("forward_receive_data_request: node_ip: {}".format(node_ip))
-    channel = None
-    for item in globals.node_connections.connection_dict.items():
-        if item[1].node_ip == node_ip:
-            channel = item[1].channel
-            break
+    #XXX
+    def forward_receive_data_request(self, node_ip, channel, request):
+        logger.info("forward_receive_data_request: node_ip: {}".format(node_ip))
 
-    traversal_stub = traversal_pb2_grpc.TraversalStub(channel)
-    response = traversal_stub.ReceiveData(
-        traversal_pb2.ReceiveDataRequest(
-                            hash_id=str(request.hash_id),
-                            request_id=str(request.request_id),
-                            stack=str(request.stack),
-                            visited=str(request.visited)))
-    logger.info("forward_receive_data_request: response: {}".format(response))
-    return response
+        traversal_stub = traversal_pb2_grpc.TraversalStub(channel)
+        response = traversal_stub.ReceiveData(
+            traversal_pb2.ReceiveDataRequest(
+                                hash_id=str(request.hash_id),
+                                request_id=str(request.request_id),
+                                stack=str(request.stack),
+                                visited=str(request.visited)))
+        logger.info("forward_receive_data_request: response: {}".format(response))
+        return response
 
-def forward_response_data(file_bytes, request_id, node_ip, status, path):
-    curr_path = eval(path)
-    curr_coordinates = curr_path.pop()
-    
-    #check if data reached the initial invoking node
-    if curr_path.empty():
-        return file_bytes
-    
-    #get the channel through which the data will be propogated
-    for item in globals.node_connections.connection_dict.items():
-        if item[1].node_coordinates == curr_coordinates:
-            channel = item[1].channel
-            break
-    
-    #check if channel is alive. if not, update the 2D matrix and calculate a new shortest path
-    if not channel.isAlive():
-        mesh = create_logical_mesh()
-        mesh[curr_coordinates[0]][curr_coordinates[1]] = 0
-        curr_path = find_shortest_path(mesh)
-        forward_response_data(file_bytes, request_id, node_ip, status, curr_path)
+    def forward_response_data(self, file_bytes, request_id, node_ip, status, path):
+        curr_path = eval(path)
+        curr_coordinates = curr_path.pop()
+        
+        #check if data reached the initial invoking node
+        if curr_path.empty():
+            return file_bytes
+        
+        #get the channel through which the data will be propogated
+        for item in globals.node_connections.connection_dict.items():
+            if item[1].node_coordinates == curr_coordinates:
+                channel = item[1].channel
+                break
+        
+        #check if channel is alive. if not, update the 2D matrix and calculate a new shortest path
+        if not channel.isAlive():
+            mesh = create_logical_mesh()
+            mesh[curr_coordinates[0]][curr_coordinates[1]] = 0
+            curr_path = find_shortest_path(mesh)
+            forward_response_data(file_bytes, request_id, node_ip, status, curr_path)
 
-    #forward the request
-    traversal_stub = traversal_pb2_grpc.TraversalStub(channel)
-    response = traversal_stub.RespondData(
-        traversal_pb2.RespondDataRequest(
-            file_bytes=file_bytes,
-            request_id=request_id,
-            node_ip=node_ip,
-            status=status,
-            path=curr_path
-        ))
-    return response
+        #forward the request
+        traversal_stub = traversal_pb2_grpc.TraversalStub(channel)
+        response = traversal_stub.RespondData(
+            traversal_pb2.RespondDataRequest(
+                file_bytes=file_bytes,
+                request_id=request_id,
+                node_ip=node_ip,
+                status=status,
+                path=curr_path
+            ))
+        return response
 
 
-#creating a 2D matrix to keep track of live and dead nodes
-def create_logical_mesh():
-    min_row = list(gossip_dictionary.values())[0][0]
-    min_col = list(gossip_dictionary.values())[0][1]
-    max_row = list(gossip_dictionary.values())[len(gossip_dictionary)-1][0]
-    max_col = list(gossip_dictionary.values())[len(gossip_dictionary)-1][1]
+    #creating a 2D matrix to keep track of live and dead nodes
+    def create_logical_mesh(self):
+        min_row = list(gossip_dictionary.values())[0][0]
+        min_col = list(gossip_dictionary.values())[0][1]
+        max_row = list(gossip_dictionary.values())[len(gossip_dictionary)-1][0]
+        max_col = list(gossip_dictionary.values())[len(gossip_dictionary)-1][1]
 
-    for key in gossip_dictionary:
-        if gossip_dictionary[key][0] < min_row:
-            min_row = gossip_dictionary[key][0]
-        if gossip_dictionary[key][1] < min_col:
-            min_col = gossip_dictionary[key][1]
-        if gossip_dictionary[key][0] > max_row:
-            max_row = gossip_dictionary[key][0]
-        if gossip_dictionary[key][1] > max_col:
-            max_col = gossip_dictionary[key][1]
+        for key in gossip_dictionary:
+            if gossip_dictionary[key][0] < min_row:
+                min_row = gossip_dictionary[key][0]
+            if gossip_dictionary[key][1] < min_col:
+                min_col = gossip_dictionary[key][1]
+            if gossip_dictionary[key][0] > max_row:
+                max_row = gossip_dictionary[key][0]
+            if gossip_dictionary[key][1] > max_col:
+                max_col = gossip_dictionary[key][1]
 
-    cols = max_col - min_col + 1
-    rows = max_row - min_col + 1
+        cols = max_col - min_col + 1
+        rows = max_row - min_col + 1
 
-    mesh = [[0]*cols]*rows
-    
-    value_list = list(gossip_dictionary.values())
-    for item in value_list:
-        mesh[item[0]][item[1]] = 1
-    
-    return mesh
+        mesh = [[0]*cols]*rows
+        
+        value_list = list(gossip_dictionary.values())
+        for item in value_list:
+            mesh[item[0]][item[1]] = 1
+        
+        return mesh
 
-def find_shortest_path(mesh):
-    path = ""
-    return path
+    def find_shortest_path(self, mesh):
+        path = ""
+        return path
