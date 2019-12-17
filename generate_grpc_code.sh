673c1ea0245cb97db60598d8bf88eb3cc6593195
Shivam Waghela <shivamwaghela@gmail.com>
2019-12-16 16:28:11 -0800

    minor change; update README.md

--- generate_grpc_code.sh
+++ generate_grpc_code.sh
@@ -15,6 +15,9 @@ then
 
     python -m grpc_tools.protoc -I./protos --python_out=./node/generated \
             --grpc_python_out=./node/generated ./protos/traversal.proto
+
+    python -m grpc_tools.protoc -I./protos --python_out=./node/generated \
+        --grpc_python_out=./node/generated ./protos/storage.proto
 else
     python3 -m grpc_tools.protoc -I./protos --python_out=./node/generated \
         --grpc_python_out=./node/generated ./protos/greet.proto
